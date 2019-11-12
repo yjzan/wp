9bbcc82b69748e9e5513431bdef8583998497d5a
yjzan <54661475+yjzan@users.noreply.github.com>
2019-11-12 10:38:32 +0800

    fixed
    fixed

--- admin-bar.js
+++ admin-bar.js
@@ -2,10 +2,10 @@
 // use jQuery and hoverIntent if loaded
 if ( typeof(jQuery) != 'undefined' ) {
     if ( typeof(jQuery.fn.hoverIntent) == 'undefined' ) {
-		/* jshint ignore:start */
+        /* jshint ignore:start */
         // hoverIntent v1.8.1 - Copy of wp-includes/js/hoverIntent.min.js
         !function(a){a.fn.hoverIntent=function(b,c,d){var e={interval:100,sensitivity:6,timeout:0};e="object"==typeof b?a.extend(e,b):a.isFunction(c)?a.extend(e,{over:b,out:c,selector:d}):a.extend(e,{over:b,out:b,selector:c});var f,g,h,i,j=function(a){f=a.pageX,g=a.pageY},k=function(b,c){return c.hoverIntent_t=clearTimeout(c.hoverIntent_t),Math.sqrt((h-f)*(h-f)+(i-g)*(i-g))<e.sensitivity?(a(c).off("mousemove.hoverIntent",j),c.hoverIntent_s=!0,e.over.apply(c,[b])):(h=f,i=g,c.hoverIntent_t=setTimeout(function(){k(b,c)},e.interval),void 0)},l=function(a,b){return b.hoverIntent_t=clearTimeout(b.hoverIntent_t),b.hoverIntent_s=!1,e.out.apply(b,[a])},m=function(b){var c=a.extend({},b),d=this;d.hoverIntent_t&&(d.hoverIntent_t=clearTimeout(d.hoverIntent_t)),"mouseenter"===b.type?(h=c.pageX,i=c.pageY,a(d).on("mousemove.hoverIntent",j),d.hoverIntent_s||(d.hoverIntent_t=setTimeout(function(){k(c,d)},e.interval))):(a(d).off("mousemove.hoverIntent",j),d.hoverIntent_s&&(d.hoverIntent_t=setTimeout(function(){l(c,d)},e.timeout)))};return this.on({"mouseenter.hoverIntent":m,"mouseleave.hoverIntent":m},e.selector)}}(jQuery);
-		/* jshint ignore:end */
+        /* jshint ignore:end */
     }
     jQuery(document).ready(function($){
         var adminbar = $('#wpadminbar'), refresh, touchOpen, touchClose, disableHoverIntent = false;
@@ -17,8 +17,19 @@ if ( typeof(jQuery) != 'undefined' ) {
         };
 
         $('#wp-admin-bar-yjz-hide-admin-bar').on('click', function (){
-            $('#wpadminbar').slideUp("slow");
+           // $('#wpadminbar').slideUp("slow");
+            $('#wpadminbar').css("top","-50px");
+            $('body').css("marginTop","0px");
+            $('#showwpadminbar').show();
         });
+
+        $('#showwpadminbar').on('click', function (){
+            // $('#wpadminbar').slideUp("slow");
+            $('#wpadminbar').css("top","0px");
+            $('body').css("marginTop","32px");
+            $('#showwpadminbar').hide();
+        });
+
 
         touchOpen = function(unbind) {
             adminbar.find('li.menupop').on('click.wp-mobile-hover', function(e) {
@@ -241,7 +252,7 @@ if ( typeof(jQuery) != 'undefined' ) {
                     }
                 }
 
-				/* remove the hover class for any objects not in the immediate element's ancestry */
+                /* remove the hover class for any objects not in the immediate element's ancestry */
                 i = q.length;
                 while ( i-- ) {
                     inA = false;
